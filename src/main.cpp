1219ad01acd8893b619fc0d1d1ec561157555571
Eric George <eric@madebymany.co.uk>
2020-04-22 09:36:39 +0100

    starting battery check

--- src/main.cpp
+++ src/main.cpp
@@ -15,10 +15,15 @@ int lastButtonState[2] = {0, 0};
 unsigned long lastDebounceTime[2] = {0, 0};
 unsigned long debounceDelay = 150;
 
+const int BATT = A13;
+const int LED = 13;
+
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting BLE work!");
+
+  pinMode(LED, OUTPUT);
 
   bleKeyboard.begin();
 
@@ -53,9 +58,19 @@ void checkButton(int idx) {
   }
 }
 
+void checkBattery() {
+  int reading = analogRead(BATT);
+  float voltage = map(reading, 0, 4095, 0.0, 4.7);
+  Serial.println(reading);
+  Serial.println(voltage);
+}
+
 void loop() {
   if(bleKeyboard.isConnected()) {
+    digitalWrite(LED, HIGH);
     checkButton(MUTE);
     checkButton(UNMUTE);
+  } else {
+    digitalWrite(LED, LOW);
   }
 }
